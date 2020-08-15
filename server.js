671ad95d6f2bcad739b2a380a3b6640a6459e2a9
Julia Bugayev <jbugayev18@gmail.com>
2020-08-14 17:16:09 -0700

    Ready For Take Off

--- server.js
+++ server.js
@@ -1,49 +1,58 @@
-'use strict';
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const helmet = require('helmet');
-const MOVIES = require('./moviedex.json');
-
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const MOVIES = require("./moviedex.json");
 
 const app = express();
+const morganOption = process.env.NODE_ENV === "production" ? "tiny" : "common";
 
-app.use(morgan('dev'));
+app.use(morgan(morganOption));
 app.use(cors());
 app.use(helmet());
 
-
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
-  const authToken = req.get('Authorization');
-  
-  if (!authToken || authToken.split(' ')[1] !== apiToken) {
-    return res.status(401).json({ error: 'Unauthorized request' });
+  const authToken = req.get("Authorization");
+
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized request" });
   }
   next();
 });
-app.get('/movie', function handleGetMovie(req,res){
+app.get("/movie", function handleGetMovie(req, res) {
   let response = MOVIES;
-  if(req.query.genre){
-    response = response.filter(movie =>
+  if (req.query.genre) {
+    response = response.filter((movie) =>
       movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
     );
   }
-  if(req.query.country) {
-    response = response.filter(movie =>
+  if (req.query.country) {
+    response = response.filter((movie) =>
       movie.country.toLowerCase().includes(req.query.country.toLowerCase())
     );
   }
-  if(req.query.avg_vote){
-    response = response.filter(movie =>
-      Number(movie.avg_vote) >= Number(req.query.avg_vote)
+  if (req.query.avg_vote) {
+    response = response.filter(
+      (movie) => Number(movie.avg_vote) >= Number(req.query.avg_vote)
     );
   }
   res.json(response);
 });
 
-const PORT = 8000;
+app.use((error, req, res, next) =>{
+  let response
+  if (process.env.NODE_ENV === 'production'){
+    response = { error: {message: 'server error'}}
+  } else {
+    response = {error}
+  }
+  res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
